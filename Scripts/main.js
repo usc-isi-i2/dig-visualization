fd9fdae9f1e0c1381e3a78f7539aa828a016faae
mahimams <me@mahimams.com>
2015-11-10 12:05:20 -0800

    Setting automatic span using query for span before timeline query

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,5 +1,5 @@
 ﻿//define(['Scripts/d3.v3', 'Scripts/elasticsearch'], function (d3, elasticsearch) {
-define(['d3', 'd3Tip', 'nvd3'], function (d3, d3Tip,nv) {
+define(['d3', 'd3Tip', 'nvd3', 'moment'], function (d3, d3Tip,nv,moment) {
     "use strict";
 
     var client = new elasticsearch.Client({
@@ -14,21 +14,10 @@ define(['d3', 'd3Tip', 'nvd3'], function (d3, d3Tip,nv) {
             }
           ]
         });
-
-    // Draw timeline based on radio button on click
-    var rad = document.timelinespan.span;
-    var prev = null;
-    for(var i = 0; i < rad.length; i++) {
-        rad[i].onclick = function() {
-            (prev)? console.log(prev.value):null;
-            if(this !== prev) {
-                prev = this;
-            }
-            var span = this.value
-            d3.selectAll("svg > *").remove();
-            drawTimeline(client,d3,d3Tip,nv,span,"");
-        };
-    }
+    
+   getSpan(client,moment).then( function(span){
+      drawTimeline(client,d3,d3Tip,nv,span,"")
+    });
 
     
 
@@ -86,11 +75,95 @@ function countInArray(array, what) {
   return count;
 }
 
+
+function getSpan(client,moment){
+  return client.search({
+    index: 'offer',
+    body: {
+        aggs : {
+            date_stats : { 
+                stats : { 
+                    field : "validFrom" 
+                    } 
+                }
+            },
+        size : 0
+    }
+
+  }).then(function(resp){
+    console.log(resp)
+    var maxDate = resp.aggregations.date_stats.max
+    var minDate = resp.aggregations.date_stats.min
+    var minMoment = moment(minDate);
+    var maxMoment = moment(maxDate);
+    var years = maxMoment.diff(minMoment,'years')
+    if (years > 3)
+      return "year"
+    else{
+      var months = maxMoment.diff(minMoment,'months')
+      if (months > 3)
+        return "month"
+      else{
+        var days = maxMoment.diff(minMoment,"days")
+        if (days > 21)
+          return "week"
+        else if (days > 3)
+          return "day"
+        else return "hour"
+      }
+    }
+  });
+}
+
+function getDateStringBySpan(dateObject,span){
+
+  var d = dateObject
+  var newDateString = ""
+  if (span == "year") {
+    newDateString = "" + d.getUTCFullYear()
+  } else if ( span == "month" ){
+      var month = d.getUTCMonth()+1;
+      if(month<10)
+          month = "" + 0 + month;
+      newDateString = d.getUTCFullYear() + "-" + month 
+  } else if ( span == "week" ){
+    d.setDate(d.getDate()-d.getUTCDay());
+    
+    var month = d.getUTCMonth()+1;
+    if(month<10)
+        month = "" + 0 + month;
+    newDateString = d.getUTCFullYear() + "-" + month + "-" + d.getUTCDate()
+  } else if ( span == "day" ){
+    var month = d.getUTCMonth()+1;
+    if(month<10)
+        month = "" + 0 + month;
+    newDateString = d.getUTCFullYear() + "-" + month + "-" + d.getUTCDate()
+  } else if (span == "hour" ){
+    var month = d.getUTCMonth()+1;
+    if(month<10)
+        month = "" + 0 + month;
+    newDateString = d.getUTCFullYear() + "-" + month + "-" + d.getUTCDate() + " " + d.getUTCHours()+":" + "00:00"  
+  }
+  return newDateString;
+}
+
+function getDateFormatBySpan(dateString,span){
+
+  if( span == "year" )
+    return d3.time.format.utc('%Y')(new Date(dateString))
+  else if (span == "month")
+    return d3.time.format.utc('%Y-%b')(new Date(dateString))
+  else if (span == "week" || span == "day")
+    return d3.time.format.utc('%Y-%b-%d')(new Date(dateString))
+  else if (span == "hour")
+    return d3.time.format.utc('%Y-%b-%d %X')(new Date(dateString))
+
+}
+
 function drawTimeline(client,d3,d3Tip,nv,span,secondQuery){
 
     // Viz for bar chart
-    if (span != "month" && span != "year")
-      span = "month"
+    
     client.msearch({
         index: 'offer',
         body:[
@@ -102,6 +175,7 @@ function drawTimeline(client,d3,d3Tip,nv,span,secondQuery){
                         date_histogram : {
                             field : "validFrom",
                             interval : span
+                            //format : "yyyy-MM-dd"
                         }
                     }
                 },
@@ -155,16 +229,8 @@ function drawTimeline(client,d3,d3Tip,nv,span,secondQuery){
                     
                     var specificDateString = fieldVal[id];
                     var d = new Date(specificDateString);
-                    var newDateString = ""
-                    if ( span == "month" ){
-                      var month = d.getUTCMonth()+1;
-                      if(month<10)
-                          month = "" + 0 + month;
-                      //specificEventIntervals.push(""+d.getFullYear()+"-"+month);
-                       newDateString = d.getUTCFullYear() + "-" + month 
-                    } else {
-                      newDateString = "" + d.getUTCFullYear()
-                    }
+                    var newDateString = getDateStringBySpan(d,span)
+                    
                     
                     specificEventIntervals.push(newDateString)
                 }
@@ -175,15 +241,7 @@ function drawTimeline(client,d3,d3Tip,nv,span,secondQuery){
         overallTimeCounts.forEach( function(obj){
             var interval = obj["key_as_string"];
             var d = new Date(interval);
-            var dateString = ""
-            if ( span == "month" ){
-              var month = d.getUTCMonth()+1;
-              if(month<10)
-                  month = "" + 0 + month;
-              dateString = "" + d.getUTCFullYear() + "-" + month
-            }else {
-              dateString = "" + d.getUTCFullYear()
-            }
+            var dateString = getDateStringBySpan(d,span)
 
             if (specificEventIntervals.indexOf(dateString) > -1) {
                 var eventCount = countInArray(specificEventIntervals,dateString);
@@ -204,7 +262,7 @@ function drawTimeline(client,d3,d3Tip,nv,span,secondQuery){
 
             nv.addGraph(function() {
               var chart = nv.models.linePlusBarChart()
-                  //.width("800")
+                  //.width("600")
                   .height("200")
                   .x(function(d,i) { return i })   
                   .y(function(d,i) { return d[1] })
@@ -217,10 +275,7 @@ function drawTimeline(client,d3,d3Tip,nv,span,secondQuery){
                     var dx = data[0].values[d] && data[0].values[d][0] || 0;
                     if (dx == 0)
                       return ""
-                    if( span == "month" )
-                      return d3.time.format.utc('%Y-%b')(new Date(dx))
-                    else
-                      return d3.time.format.utc('%Y')(new Date(dx))
+                    return getDateFormatBySpan(dx,span)
               });
 
               chart.useInteractiveGuideline = true;
@@ -230,26 +285,20 @@ function drawTimeline(client,d3,d3Tip,nv,span,secondQuery){
                     var dx = data[0].values[d] && data[0].values[d][0] || 0;
                     if (dx == 0)
                       return ""
-                    if( span == "month" )
-                      return d3.time.format.utc('%Y-%b')(new Date(dx))
-                    else
-                      return d3.time.format.utc('%Y')(new Date(dx))  });
+                    return getDateFormatBySpan(dx,span)  
+                  });
 
               chart.tooltipContent(function(d){
                 if (d.point) {
                   var dx = d.point[0]
                   if (dx == 0)
                       return ""
-                  if( span == "month" )
-                    return "Specific event " + d3.time.format.utc('%Y-%b')(new Date(dx)) + " : " + d.point[1]
-                  else
-                     return "Specific event " + d3.time.format.utc('%Y')(new Date(dx)) + " : " + d.point[1]
+                  return "Specific Event " + getDateFormatBySpan(dx,span) + " : " + d.point[1]
                 }else if ( d.data ){
                   var dx = d.data[0]
-                  if( span == "month" )
-                    return " Overall timeline " + d3.time.format.utc('%Y-%b')(new Date(dx)) + " : " + d.data[1]
-                  else
-                    return " Overall timeline " + d3.time.format.utc('%Y')(new Date(dx)) + " : " + d.data[1]
+                  if (dx == 0)
+                      return ""
+                  return "Overall timeline " + getDateFormatBySpan(dx,span) + " : " + d.data[1]
                 }
 
               })
